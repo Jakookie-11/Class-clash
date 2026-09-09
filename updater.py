092e1192afdee07de29aaa4b759bfc95c00c51e1
Jakob Samstag <jakob.samstag@gmail.com>
2026-09-09 12:04:07 +0200

    v1.37-t

--- updater.py
+++ updater.py
@@ -2,25 +2,25 @@ import os
 import shutil
 
 
-for datei in os.listdir("Class-clash/"):
+for datei in os.listdir("."):
     if datei.endswith(".py") and datei != "updater.py":
-        os.remove("Class-clash/" + datei)
+        os.remove(datei)
     elif datei.endswith(".txt"):
-        os.remove("Class-clash/" + datei)
+        os.remove(datei)
     elif datei.endswith(".json"):
-        os.remove("Class-clash/" + datei)
-    elif datei.endswith(".zib"):
-        os.remove("Class-clash/" + datei)
+        os.remove(datei)
+    elif datei.endswith(".zip"):
+        os.remove(datei)
 
 
-shutil.rmtree("Class-clash/__pycache__")
+shutil.rmtree("__pycache__")
 
 
-for datei in os.listdir("Class-clash/update/Class-clash-Version-information-and-asking"):
+for datei in os.listdir("update/Class-clash-Version-information-and-asking"):
     shutil.move(
-        "Class-clash/update/Class-clash-Version-information-and-asking/" + datei,
-        "Class-clash/"
+        "update/Class-clash-Version-information-and-asking/" + datei,
+        "."
     )
 
 
-shutil.rmtree("Class-clash/update/")
+shutil.rmtree("update/")
