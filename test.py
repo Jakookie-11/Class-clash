ebb1a92f57d50bfc84289f668ad8e4e08afaa5e2
Jakob Samstag <jakob.samstag@gmail.com>
2026-09-16 21:48:41 +0200

    Update version to 1.02.02-beta; remove .gitignore from update directory; clean up __pycache__ if it exists

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
 #----------Tests----------#
+#(.venv) PS C:\Users\jakob\Desktop\Git\Class clash>                       git config core.autocrlf true
+#(.venv) PS C:\Users\jakob\Desktop\Git\Class clash>                       git add --renormalize .
+#(.venv) PS C:\Users\jakob\Desktop\Git\Class clash>                       git status
