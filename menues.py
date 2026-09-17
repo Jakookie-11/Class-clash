7df51365e3152b477e2a016e431f3d816e6575e3
Jakob Samstag <jakob.samstag@gmail.com>
2026-09-17 16:45:42 +0200

    Update menu options to use consistent numbering format

--- menues.py
+++ menues.py
@@ -1,55 +1,55 @@
 
 
 hauptmenue = {
-    "1) Spiel Starten" : "",
-    "2) Charaktere"    : "",
-    "3) Einstellungen" : "",
-    "4) Shop         " : "",
-    "5) Beenden"       : ""
+    "[1] Spiel Starten" : "",
+    "[2] Charaktere"    : "",
+    "[3] Einstellungen" : "",
+    "[4] Shop         " : "",
+    "[5] Beenden"       : ""
     
 }
 
 
 charakter_bip_menue = {
-    "1) Anzeigen" : "",
-    "2) Leveln"   : "",
-    "3) Zurueck"  : ""
+    "[1] Anzeigen" : "",
+    "[2] Leveln"   : "",
+    "[3] Zurueck"  : ""
 }
 
 
 shop_menue = {
-    "1) 1000 Credits   " : "",
-    "2) 20   Material 1" : "",
-    "3) 20   Energie   " : "",
-    "4) Zurueck        " : ""
+    "[1] 1000 Credits   " : "",
+    "[2] 20   Material 1" : "",
+    "[3] 20   Energie   " : "",
+    "[4] Zurueck        " : ""
 }
 
 
 neues_spiel_menue = {
-    "1) benutzer-definieter Kampf" : "",
-    "2) Kampange(n)"               : "",
-    "3) Zurueck"                   : ""
+    "[1] benutzer-definieter Kampf" : "",
+    "[2] Kampange(n)"               : "",
+    "[3] Zurueck"                   : ""
 }
 
 
 anmelden_menue = {
-    "1) Anmelden"     : "",
-    "2) Registrieren" : "",
-    "3) Admin"        : "",
+    "[1] Anmelden"     : "",
+    "[2] Registrieren" : "",
+    "[3] Admin"        : "",
 }
 
 
 einstellungen_menue = {
-    "1) Profil"         : "",
-    "2) Credits"        : "",
-    "3) Wichtige Links" : "",
-    "4) Zurueck"        : ""
+    "[1] Profil"         : "",
+    "[2] Credits"        : "",
+    "[3] Wichtige Links" : "",
+    "[4] Zurueck"        : ""
 }
 #->Y
 profil_menue = {
-    "1) Passwort aendern"     : "",
-    "2) Profil Daten zeigen"  : "",
-    "3) Profil Zuruecksetzen" : "",
-    "4) Profil Loeschen"      : "",
-    "5) Zurueck"              : ""
+    "[1] Passwort aendern"     : "",
+    "[2] Profil Daten zeigen"  : "",
+    "[3] Profil Zuruecksetzen" : "",
+    "[4] Profil Loeschen"      : "",
+    "[5] Zurueck"              : ""
 }
